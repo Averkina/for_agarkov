ec2ccbc64bd6f9591830d1b04cdf6e4affba8c64
Maria <Admin@Admin-PC>
2014-02-15 22:14:45 +0400

    Task2 - исправленная и более современная версия

--- src/Task2/Module.java
+++ src/Task2/Module.java
@@ -1,94 +1,119 @@
 package Task2;
 
-import java.util.ArrayList;
 import java.util.*;
+
+
 
 public class Module {
 
-    public String ID;
-    public ArrayList<String> deps;
+    public final String id;
+    public final String[] depends;
 
-    Module(String newID, String... newDependencies) {
-        this.ID = newID;
-        deps = new ArrayList<String>();
-        for (int i = 0; i < newDependencies.length; ++i) {
-            deps.add(newDependencies[i]);
+    Module(String id, String... depends) {
+        this.id = id;
+        this.depends = depends;
+    }
+
+
+    public static boolean existsId(String targetId, ArrayList<Module> modules) {
+        for (int idIndex = 0; idIndex < modules.size(); idIndex++) {
+            String nextId = modules.get(idIndex).id;
+//            System.out.println("depends: " + targetId + " and id: " + nextId);
+            if (targetId.equals(nextId)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean canResolveDependence(ArrayList<Module> modules, int moduleIndex) {
+        Module module = modules.get(moduleIndex);
+        for (int dependsIndex = 0; dependsIndex < module.depends.length; dependsIndex++) {
+            String dependenceId = module.depends[dependsIndex];
+            if (!existsId(dependenceId, modules)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void print(ArrayList<Module> modules) {
+
+        Iterator<Module> itr = modules.iterator(); 
+        while (itr.hasNext()) {
+            Module element = itr.next();
+            System.out.print(element.id + " ");
+            for (final String depend : element.depends) {
+                System.out.print("[" + depend + "]");
+            }
+            System.out.println();
         }
     }
 
-    Module(String newID, ArrayList<String> newDeps) {
-        this.ID = newID;
-        this.deps = newDeps;
+
+    public static int removeUnresolvedModules(ArrayList<Module> modules) {
+        int removed = 0;
+        for (int moduleIndex = 0; moduleIndex < modules.size(); moduleIndex++) {
+            if (canResolveDependence(modules, moduleIndex) == false) {
+//                System.out.println("remove: " + modules.get(moduleIndex).id);
+                modules.remove(moduleIndex);
+                removed++;
+            }
+        }
+        return removed;
     }
+    
+    public static boolean hasLiveDependepcies(Module module, ArrayList<Module> modules) {
+        for (String dep : module.depends) {
+            if (existsId(dep, modules)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public static void findAndPrintAndRemoveIndependentModules(ArrayList<Module> modules) {
+        Iterator<Module> it = modules.iterator(); 
+        while (it.hasNext()) {
+            Module module = it.next();
+            if (!hasLiveDependepcies(module, modules)) {
+                System.out.print(module.id + ", ");
+                it.remove();
+            }
+        }
+    }
+    
+
 
     public static void main(String[] args) {
 
-        ArrayList<Module> modules = new ArrayList<Module>();
+        ArrayList<Module> moduleList = new ArrayList<Module>();
 
-        modules.add(new Module("1", ""));
-        modules.add(new Module("2", "1"));
-        modules.add(new Module("3", "2"));
-        modules.add(new Module("4", "3", "2"));
-        modules.add(new Module("5", "1", "4"));
-        modules.add(new Module("61", "1", "70"));
-        modules.add(new Module("6", "5"));
+        moduleList.add(new Module("1"));
+        moduleList.add(new Module("2", "1"));
+        moduleList.add(new Module("3", "2"));
+        moduleList.add(new Module("4", "3", "2"));
+        moduleList.add(new Module("5", "1", "4"));
+        moduleList.add(new Module("61", "1", "70"));
+        moduleList.add(new Module("6", "5"));
 
-        Collections.shuffle(modules);
+//        moduleList.add(new Module("1"));
+//        moduleList.add(new Module("2", "1"));
+//        moduleList.add(new Module("A", "1", "B"));
+//        moduleList.add(new Module("B", "X"));
 
-        boolean anythingMoved;
-        do {
-            anythingMoved = false;
-            for (int i = 0; i < modules.size(); ++i) {
-                String myId = modules.get(i).ID;
-                int newPos = -1;
-                for (int j = i + 1; j < modules.size(); ++j) {
-                    if (modules.get(j).deps.contains(myId)) {
-                        newPos = j; 
-                    }
-                }
-
-                if (newPos != -1) {
-                    Module m = new Module(modules.get(newPos).ID, modules.get(newPos).deps);
-                    modules.set(newPos, modules.get(i));
-                    modules.set(i, m);
-
-                    anythingMoved = true;
-                }
-            }
+        while (removeUnresolvedModules(moduleList) > 0) {
         }
-
-        while (anythingMoved);
-
-        Collections.reverse(modules);
-
-        System.out.println("Sorted before removal of the unnecessary");
-        for (int i = 0; i < modules.size(); ++i) {
-            System.out.println(modules.get(i).ID);
+        
+        //print(moduleList);
+        
+        while (!moduleList.isEmpty()) {
+            findAndPrintAndRemoveIndependentModules(moduleList);
         }
-
-        for (int i = 0 + 1; i < modules.size(); ++i) {
-            for (int j = 0; j < modules.get(i).deps.size(); ++j) {
-                String myDep = modules.get(i).deps.get(j);
-                boolean resolved = false;
-
-                for (int prev = 0; prev < i && !resolved; ++prev) {
-                    if (modules.get(prev).ID.equals(myDep)) {
-                        resolved = true;
-                    }
-                }
-
-                if (!resolved) {
-                    System.out.println("Module with id " + modules.get(i).ID + " was removed");
-                    modules.remove(i);
-                    --i;
-                    break;
-                }
-            }
-        }
-
-        System.out.println("Result:");
-        for (int i = 0; i < modules.size(); ++i) {
-            System.out.println(modules.get(i).ID);
-        }
+        
+        //print(moduleList);
     }
 }
+
+
+
