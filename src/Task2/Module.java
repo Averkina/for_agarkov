86d437c48c7c2f29e5dc3a7fbcddd0a0b295ef07
Maria <Admin@Admin-PC>
2014-02-23 01:13:32 +0400

    ADD: Решение с использованием HashMap

--- src/Task2/Module.java
+++ src/Task2/Module.java
@@ -2,118 +2,99 @@ package Task2;
 
 import java.util.*;
 
-
-
 public class Module {
 
-    public final String id;
-    public final String[] depends;
+    public String[] depends;
 
-    Module(String id, String... depends) {
-        this.id = id;
+    Module(String... depends) {
         this.depends = depends;
     }
 
-
-    public static boolean existsId(String targetId, ArrayList<Module> modules) {
-        for (int idIndex = 0; idIndex < modules.size(); idIndex++) {
-            String nextId = modules.get(idIndex).id;
-//            System.out.println("depends: " + targetId + " and id: " + nextId);
-            if (targetId.equals(nextId)) {
-                return true;
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        for (String depend : depends) {
+            if (builder.length() > 0) {
+                builder.append("_");
             }
+            builder.append(depend);
         }
-        return false;
+        return builder.toString();
     }
 
-    public static boolean canResolveDependence(ArrayList<Module> modules, int moduleIndex) {
-        Module module = modules.get(moduleIndex);
-        for (int dependsIndex = 0; dependsIndex < module.depends.length; dependsIndex++) {
-            String dependenceId = module.depends[dependsIndex];
-            if (!existsId(dependenceId, modules)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static void print(ArrayList<Module> modules) {
-
-        Iterator<Module> itr = modules.iterator(); 
-        while (itr.hasNext()) {
-            Module element = itr.next();
-            System.out.print(element.id + " ");
-            for (final String depend : element.depends) {
-                System.out.print("[" + depend + "]");
-            }
-            System.out.println();
-        }
-    }
-
-
-    public static int removeUnresolvedModules(ArrayList<Module> modules) {
-        int removed = 0;
-        for (int moduleIndex = 0; moduleIndex < modules.size(); moduleIndex++) {
-            if (canResolveDependence(modules, moduleIndex) == false) {
-//                System.out.println("remove: " + modules.get(moduleIndex).id);
-                modules.remove(moduleIndex);
-                removed++;
-            }
-        }
-        return removed;
-    }
-    
-    public static boolean hasLiveDependepcies(Module module, ArrayList<Module> modules) {
-        for (String dep : module.depends) {
-            if (existsId(dep, modules)) {
-                return true;
-            }
-        }
-        return false;
-    }
-    
-    public static void findAndPrintAndRemoveIndependentModules(ArrayList<Module> modules) {
-        Iterator<Module> it = modules.iterator(); 
+    public static void print(ArrayList<String> id) {
+        Iterator<String> it = id.iterator(); 
         while (it.hasNext()) {
-            Module module = it.next();
-            if (!hasLiveDependepcies(module, modules)) {
-                System.out.print(module.id + ", ");
-                it.remove();
+            String idList = it.next();
+            System.out.print(idList + " ");
+        }
+    }
+
+    public static void removeUnresolvedModules(HashMap<String, Module> modules) {
+        Iterator<Map.Entry<String, Module>> it = modules.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<String, Module> entry = it.next();
+            Module module = entry.getValue();
+            for (String depend : module.depends) {
+                if (!modules.containsKey(depend)) {
+                    it.remove();
+                }
             }
         }
     }
-    
 
+    public static ArrayList<String> findAndPrintAndRemoveIndependentModules(HashMap<String, Module> modules) {
+        Iterator<Map.Entry<String, Module>> it = modules.entrySet().iterator();
+        ArrayList<String> idList = new ArrayList<String>();
+        while (it.hasNext()) {
+            Map.Entry<String, Module> entry = it.next();
+            Module module = entry.getValue();
+            if (!containsExistsDependence(module, modules)) {
+                String id = entry.getKey();
+                it.remove();
+                idList.add(id);
+            }
+        }
+        return idList;
+    }
+
+    public static boolean containsExistsDependence(Module module, HashMap<String, Module> modules) {
+        for (String depend : module.depends) {
+            if (modules.containsKey(depend)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     public static void main(String[] args) {
 
-        ArrayList<Module> moduleList = new ArrayList<Module>();
+        HashMap<String, Module> modules = new HashMap<String, Module>();
 
-        moduleList.add(new Module("1"));
-        moduleList.add(new Module("2", "1"));
-        moduleList.add(new Module("3", "2"));
-        moduleList.add(new Module("4", "3", "2"));
-        moduleList.add(new Module("5", "1", "4"));
-        moduleList.add(new Module("61", "1", "70"));
-        moduleList.add(new Module("6", "5"));
+        modules.put("1", new Module());
+        modules.put("2", new Module("1"));
+        modules.put("3", new Module("2"));
+        modules.put("4", new Module("3", "2"));
+        modules.put("5", new Module("1", "4"));
+        modules.put("61", new Module("1", "70"));
+        modules.put("6", new Module("5"));
 
-//        moduleList.add(new Module("1"));
-//        moduleList.add(new Module("2", "1"));
-//        moduleList.add(new Module("A", "1", "B"));
-//        moduleList.add(new Module("B", "X"));
 
-        while (removeUnresolvedModules(moduleList) > 0) {
+        //        modules.put("1", new Module());
+        //        modules.put("2", new Module("1"));
+        //        modules.put("A", new Module("1", "B"));
+        //        modules.put("B", new Module("X"));
+
+        System.out.println("Modules with dependences:");
+        System.out.println(modules.toString());
+        System.out.println("\nModules without the resolved dependences:");
+
+        removeUnresolvedModules(modules);
+
+        System.out.println(modules.toString());
+        System.out.println("\nId resolved dependences:");
+
+        while (!modules.isEmpty()) {
+            print(findAndPrintAndRemoveIndependentModules(modules));
         }
-        
-        //print(moduleList);
-        
-        while (!moduleList.isEmpty()) {
-            findAndPrintAndRemoveIndependentModules(moduleList);
-        }
-        
-        //print(moduleList);
     }
 }
-
-
-
